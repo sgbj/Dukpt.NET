14d789c7c935c2f78e7dc885ba1ea5798d71d18b
sgbj <scott.batary@gmail.com>
2013-08-30 15:18:50 -0500

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -17,12 +17,12 @@ namespace Test
             var bdk = "0123456789ABCDEFFEDCBA9876543210";
             var ksn = "FFFF9876543210E00008";
             var track = "C25C1D1197D31CAA87285D59A892047426D9182EC11353C051ADD6D0F072A6CB3436560B3071FC1FD11D9F7E74886742D9BEE0CFD1EA1064C213BB55278B2F12";
-            var decBytes = Dukpt.Encrypt(bdk, ksn, BigInt.FromHex(track).GetBytes(), false);
+            var decBytes = Dukpt.Decrypt(bdk, ksn, BigInt.FromHex(track).GetBytes());
             var decrypted = UTF8Encoding.UTF8.GetString(decBytes);
             Console.WriteLine(decrypted == test);
 
             // Encrypting
-            var encBytes = Dukpt.Encrypt(bdk, ksn, decBytes, true);
+            var encBytes = Dukpt.Encrypt(bdk, ksn, decBytes);
             var encrypted = BitConverter.ToString(encBytes).Replace("-", "");
             Console.WriteLine(encrypted == track);
         }
