02b5cb62ca7d44c0a7b1346618922f14de376b36
sgbj <scott.batary@gmail.com>
2013-08-30 15:14:35 -0500

    Update Dukpt.cs

--- Dukpt/Dukpt.cs
+++ Dukpt/Dukpt.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Text;
 using System.Linq;
 using System.Numerics;
@@ -24,8 +24,8 @@ namespace DukptSharp
 
         public static BigInteger CreateIpek(BigInteger ksn, BigInteger bdk)
         {
-            return Encrypt("TripleDES", true, bdk, (ksn & KsnMask) >> 16) << 64 
-                 | Encrypt("TripleDES", true, bdk ^ KeyMask, (ksn & KsnMask) >> 16);
+            return Transform("TripleDES", true, bdk, (ksn & KsnMask) >> 16) << 64 
+                 | Transform("TripleDES", true, bdk ^ KeyMask, (ksn & KsnMask) >> 16);
         }
 
         public static BigInteger CreatePek(BigInteger ipek, BigInteger ksn)
@@ -38,13 +38,8 @@ namespace DukptSharp
             var ksnReg = ksn & Ls16Mask & Reg8Mask;
             var curKey = ipek;
             for (var shiftReg = ShiftRegMask; shiftReg > 0; shiftReg >>= 1)
-            {
                 if ((shiftReg & ksn & Reg3Mask) > 0)
-                {
-                    ksnReg |= shiftReg;
-                    curKey = GenerateKey(curKey, ksnReg);
-                }
-            }
+                    curKey = GenerateKey(curKey, ksnReg |= shiftReg);
             return curKey;
         }
 
@@ -55,10 +50,10 @@ namespace DukptSharp
 
         public static BigInteger EncryptRegister(BigInteger curKey, BigInteger reg8)
         {
-            return (curKey & Ls16Mask) ^ Encrypt("DES", true, (curKey & Ms16Mask) >> 64, (curKey & Ls16Mask ^ reg8));
+            return (curKey & Ls16Mask) ^ Transform("DES", true, (curKey & Ms16Mask) >> 64, (curKey & Ls16Mask ^ reg8));
         }
 
-        public static BigInteger Encrypt(string name, bool encrypt, BigInteger key, BigInteger message)
+        public static BigInteger Transform(string name, bool encrypt, BigInteger key, BigInteger message)
         {
             using (var cipher = SymmetricAlgorithm.Create(name))
             {
@@ -67,38 +62,23 @@ namespace DukptSharp
                 cipher.Mode = CipherMode.CBC;
                 cipher.Padding = PaddingMode.Zeros;
                 using (var crypto = encrypt ? cipher.CreateEncryptor() : cipher.CreateDecryptor())
-                using (var ms = new MemoryStream())
                 {
                     var data = message.GetBytes();
-                    BigInteger x = 0;
-                    for (var i = 0; i < data.Length / 8; i++)
-                    {
-                        var bi = BigInt.FromBytes(message.GetBytes().Skip(i * 8).Take(8).ToArray());
-                        if (encrypt)
-                        {
-                            bi = BigInt.FromBytes(crypto.TransformFinalBlock((bi ^ x).GetBytes(), 0, 8));
-                            x = bi;
-                        }
-                        else
-                        {
-                            var y = bi;
-                            bi = BigInt.FromBytes(crypto.TransformFinalBlock(bi.GetBytes(), 0, 8)) ^ x;
-                            x = y;
-                        }
-                        var biData = bi.GetBytes();
-                        ms.Write(biData, 0, biData.Length);
-                    }
-                    return BigInt.FromBytes(ms.ToArray());
+                    return BigInt.FromBytes(crypto.TransformFinalBlock(data, 0, data.Length));
                 }
             }
         }
 
-        public static byte[] Encrypt(string bdk, string ksn, byte[] track, bool encrypt = true)
+        public static byte[] Encrypt(string bdk, string ksn, byte[] track)
         {
-            var ipek = Dukpt.CreateIpek(BigInt.FromHex(ksn), BigInt.FromHex(bdk));
-            var pek = Dukpt.CreatePek(ipek, BigInt.FromHex(ksn));
-            var encrypted = Dukpt.Encrypt("TripleDES", encrypt, pek, BigInt.FromBytes(track));
-            return encrypted.GetBytes();
+            return Transform("TripleDES", true, CreatePek(CreateIpek(
+                BigInt.FromHex(ksn), BigInt.FromHex(bdk)), BigInt.FromHex(ksn)), BigInt.FromBytes(track)).GetBytes();
+        }
+
+        public static byte[] Decrypt(string bdk, string ksn, byte[] track)
+        {
+            return Transform("TripleDES", false, CreatePek(CreateIpek(
+                BigInt.FromHex(ksn), BigInt.FromHex(bdk)), BigInt.FromHex(ksn)), BigInt.FromBytes(track)).GetBytes();
         }
     }
 }
